b055473fbbad415b16b210ad9eee178c41686a95
abigailprowse <79606542+abigailprowse@users.noreply.github.com>
2023-03-07 16:55:09 -0500

    add beginnings of HOTAS Joystick class

---
+++ src/main/java/frc/robot/usercontrol/HOTASJoystick.java
@@ -0,0 +1,21 @@
+/*
+ * Made by: Abigail Prowse
+ * Started on: March 7, 2023
+ * Purpose: Class for gathering input data from HOTAS Flightstick
+ * Package: frc.robot.usercontrol
+ * 
+ * Last Edited On: March 7, 2023
+ * Change Log:
+ * 3/7/23 - add STICK_AXIS_X and STICK_Y_AXIS
+ */
+
+package frc.robot.usercontrol;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class HOTASJoystick {
+    
+    // defines the axes for each button, slider, and joystick on the flight pad
+    public static final int STICK_AXIS_X = 0;
+    public static final int STICK_AXIS_Y = 0;
+}
