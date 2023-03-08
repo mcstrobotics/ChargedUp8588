72b59fa0760df795315ede6c63f26b5f0ae4ffc7
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2023-03-08 21:20:15 +0000

    Extending Joystick class

--- src/main/java/frc/robot/usercontrol/HOTASJoystick.java
+++ src/main/java/frc/robot/usercontrol/HOTASJoystick.java
@@ -9,13 +9,22 @@
  * 3/7/23 - add STICK_AXIS_X and STICK_Y_AXIS
  */
 
+ // Last Edited On: March 8, 2023 by Rohan Parikh
+ // Change Log:
+ // Extended Joystick class to allow for easier access to HOTAS flightstick
+ // Joystick class already also comes with built in methods for getting axis and button values for flightstick
+
 package frc.robot.usercontrol;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-public class HOTASJoystick {
+public class HOTASJoystick extends Joystick{
     
     // defines the axes for each button, slider, and joystick on the flight pad
     public static final int STICK_AXIS_X = 0;
     public static final int STICK_AXIS_Y = 0;
+    public HOTASJoystick(int port) {
+        super(port);
+    }
+    
 }
