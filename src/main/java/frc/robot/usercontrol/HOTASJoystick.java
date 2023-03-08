1cd5b3b904f284f8d17317a378fc93ca32a683d0
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2023-03-08 21:42:44 +0000

    Added all 5 axises static final int variables in HOTASJoystick.java

--- src/main/java/frc/robot/usercontrol/HOTASJoystick.java
+++ src/main/java/frc/robot/usercontrol/HOTASJoystick.java
@@ -21,10 +21,16 @@ import edu.wpi.first.wpilibj.Joystick;
 public class HOTASJoystick extends Joystick{
     
     // defines the axes for each button, slider, and joystick on the flight pad
-    public static final int STICK_AXIS_X = 0;
-    public static final int STICK_AXIS_Y = 0;
+    public static final int STICK_AXIS_X = 0; // axis 0
+    public static final int STICK_AXIS_Y = 0; //axis 2
+    public static final int SMALL_BUTTON_X = 0; //axis 4
+    public static final int SMALL_BUTTON_Y = 0; //axis 2
+    public static final int TURNER_AXIS = 0; //axis 3
+
+    // now the buttons, who's description is purely in the eye of the beholder 
+    
     public HOTASJoystick(int port) {
         super(port);
     }
-    
+
 }
